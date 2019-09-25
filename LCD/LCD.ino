0f1d8cce7d69800d33a64fb083ea8a5d79333026
Alden L Dent <adent11@ccs.local>
2019-09-25 15:24:08 -0400

    commented Hello LCD

--- LCD/LCD.ino
+++ LCD/LCD.ino
@@ -1,23 +1,23 @@
+// Alden Dent Hello LCD
+// Prints hello world and time since code began
 #include <LiquidCrystal.h>
-
 LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
 unsigned long time;
-void setup() {
-  lcd.begin(16, 2);
-  Serial.begin(9600);
-
+void setup()
+{
+	lcd.begin(16, 2);
+	Serial.begin(9600);
 }
 
-void loop() {
-lcd.setCursor(0, 0);
-lcd.print("hello world          ");
-lcd.setCursor(0, 1);
-lcd.print("Time: ");
-time = millis();
-lcd.print(time/1000);
-lcd.print("            ");
-Serial.print(time/1000);
-delay(1000);
-//lcd.clear();
+void loop()
+{
+	lcd.setCursor(0, 0);
+	lcd.print("hello world          ");
+	lcd.setCursor(0, 1);
+	lcd.print("Time: ");
+	time = millis();
+	lcd.print(time/1000);
+	lcd.print("            ");
+	Serial.print(time/1000);
+	delay(1000);
 }
-
